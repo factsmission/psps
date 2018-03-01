e472f8d7ec851ae70e68de185c551eb1d87888a3
Reto Gmür <reto@factsmission.com>
2018-03-02 00:07:17 +0100

    Added GET support to Sparql Proxy

--- src/main/java/com/factsmission/psps/server/SparqlProxy.java
+++ src/main/java/com/factsmission/psps/server/SparqlProxy.java
@@ -23,44 +23,27 @@
  */
 package com.factsmission.psps.server;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Iterator;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
 
-import com.factsmission.psps.Ontology;
-import com.factsmission.psps.UploadRepoGraph;
-import com.factsmission.psps.UploadRepoGraphArgs;
-
-import org.apache.clerezza.commons.rdf.IRI;
-import org.apache.clerezza.commons.rdf.RDFTerm;
 import org.apache.clerezza.rdf.utils.GraphNode;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.ConnectionClosedException;
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.ProtocolException;
-import org.apache.http.client.RedirectStrategy;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.util.EntityUtils;
 
 import solutions.linked.slds.ConfigUtils;
 
@@ -86,10 +69,35 @@ public class SparqlProxy {
     public Response handlePost(@Context HttpHeaders httpHeaders, InputStream body) throws Exception {
         final HttpPost httpPost = new HttpPost(
             configUtils.getSparqlEndpointUri().getUnicodeString());
-        final String mediaType = httpHeaders.getHeaderString("Content-Type");
-        httpPost.setHeader("Content-Type", mediaType);
+        final String contentType = httpHeaders.getHeaderString("Content-Type");
+        httpPost.setHeader("Content-Type", contentType);
+        final String acceptType = httpHeaders.getHeaderString("Accept");
+        httpPost.setHeader("Accept", acceptType);
         httpPost.setEntity(new InputStreamEntity(body));    
         try (CloseableHttpResponse upResponse = httpclient.execute(httpPost)) {
+            ResponseBuilder builder = Response.status(upResponse.getStatusLine().getStatusCode());
+            HttpEntity upEntity = upResponse.getEntity();
+            builder.header("Content-Type", upEntity.getContentType().getValue());
+            byte[] bytes = null;
+            try {
+                bytes = IOUtils.toByteArray(upEntity.getContent());
+            } catch (ConnectionClosedException ex) {
+                System.err.println("Reading answer from fuseki: "+ex.toString());
+            }
+            builder.entity(bytes);//new ByteArrayInputStream("huolllo".getBytes()));//
+            return builder.build();
+        }        
+    }
+
+    @GET
+    public Response handleGet(@Context HttpHeaders httpHeaders, @Context UriInfo uriInfo) throws Exception {
+        String fullRequestURI = uriInfo.getRequestUri().toString();
+        String queryPart = fullRequestURI.substring(fullRequestURI.indexOf('?'));
+        final HttpGet httpGet = new HttpGet(
+            configUtils.getSparqlEndpointUri().getUnicodeString()+queryPart);  
+        final String acceptType = httpHeaders.getHeaderString("Accept");
+        httpGet.setHeader("Accept", acceptType);
+        try (CloseableHttpResponse upResponse = httpclient.execute(httpGet)) {
             ResponseBuilder builder = Response.status(upResponse.getStatusLine().getStatusCode());
             HttpEntity upEntity = upResponse.getEntity();
             builder.header("Content-Type", upEntity.getContentType().getValue());
